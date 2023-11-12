b7d3b6eb6a9211787df8b5de93be6cc7ec111582
ibelauste <115626260+ibelauste@users.noreply.github.com>
2023-11-11 18:51:22 -0600

    prueba.py

--- prueba.py
+++ prueba.py
@@ -4,22 +4,12 @@ from dash.dependencies import Input, Output
 import pandas as pd
 import base64
 import io
-import os
 import app
-import server
 
 # Inicia la aplicación Dash con el nombre 'prueba'
 prueba = dash.Dash(__name__)
 
-# Variable global para almacenar los datos del CSV
-global_data = None
-
-# Nombre del archivo CSV para almacenar los datos
-csv_filename = 'data.csv'
-
-# Verifica si el archivo CSV ya existe, si no, crea uno vacío
-if not os.path.exists(csv_filename):
-    pd.DataFrame(columns=['Year', 'Global_Sales']).to_csv(csv_filename, index=False)
+server = app.server
 
 # Diseño del layout
 prueba.layout = html.Div([
@@ -37,26 +27,16 @@ prueba.layout = html.Div([
 
 # Función para cargar el archivo CSV y procesar los datos
 def parse_contents(contents, filename):
-    global global_data
-
     content_type, content_string = contents.split(',')
 
     # Decodifica el contenido base64 en datos binarios
     decoded = base64.b64decode(content_string)
 
     # Lee los datos del CSV
-    new_data = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
-
-    # Guarda los nuevos datos en el archivo CSV
-    current_data = pd.read_csv(csv_filename)
-    updated_data = pd.concat([current_data, new_data], ignore_index=True)
-    updated_data.to_csv(csv_filename, index=False)
-
-    # Actualiza la variable global con los datos combinados
-    global_data = updated_data
+    df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
 
     # Agrupa por año y suma las ventas globales
-    df_grouped = global_data.groupby('Year')['Global_Sales'].sum().reset_index()
+    df_grouped = df.groupby('Year')['Global_Sales'].sum().reset_index()
 
     return df_grouped
 
@@ -65,8 +45,6 @@ def parse_contents(contents, filename):
               [Input('upload-data', 'contents'),
                Input('upload-data', 'filename')])
 def update_graph(contents, filename):
-    global global_data
-
     if contents is None:
         return dash.no_update
 
@@ -87,4 +65,4 @@ def update_graph(contents, filename):
     return fig
 
 if __name__ == '__main__':
-    prueba.run_server(debug=True, port=8051)
+    prueba.run_server(debug=True)
