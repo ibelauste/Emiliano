95c54709218fc3238348b99770609cc2980eecaf
ibelauste <115626260+ibelauste@users.noreply.github.com>
2023-11-11 18:26:00 -0600

    prueba_emiliano.py

--- prueba_emiliano.py
+++ prueba_emiliano.py
@@ -10,8 +10,6 @@ import app
 # Inicia la aplicación Dash con el nombre 'prueba'
 prueba = dash.Dash(__name__)
 
-server = app.server
-
 # Variable global para almacenar los datos del CSV
 global_data = None
 
